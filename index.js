8a6995fe5159256730a3a2a384a08af9b073e764
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-06-01 18:35:58 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "James Bond",
+    streetAddress: "CloudPlatform8, server-7, Dallas, Texas."
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmpObj = {...employee}
+    newEmpObj[key] =value;
+    return newEmpObj;
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+   employee[key] = value;
+   return employee;
+
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmpObj = {...employee}
+    delete newEmpObj[key];
+    return newEmpObj;
+ 
+ }
+ function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+ 
+ }
